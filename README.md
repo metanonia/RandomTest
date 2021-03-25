689669d3d72fdfd533ec5b55145a868005343269
metanonia <metanonia@outlook.kr>
2021-03-25 17:23:17 +0900

    2.1 Frequency (Monobit) Test

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ BlockHash Random Test
 
 이더리움 블록해시의 랜덤성 테스트용
 NIST statistical Test Suite (https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-22r1a.pdf) 
+
+BlockHash.txt: Ethereum Mainnet Blockhash(11000000 ~ 12000000)
--- src/main/java/com/metanonia/random/Frequency.java
+++ src/main/java/com/metanonia/random/Frequency.java
@@ -1,5 +1,9 @@
 package com.metanonia.random;
 
+/**********
+ * 2.1 Frequency (Monobit) Test
+ */
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,25 +17,35 @@ import static java.lang.Math.sqrt;
 public class Frequency {
     public static void main(String[] args) {
         try {
-            File file = new File(args[0]);
+            File file = new File("./BlockHash.txt");
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = "";
             Integer sum = 0;
             Integer count = 0;
+            Integer lineCount = 0;
             while ((line = bufferedReader.readLine()) != null) {
                 if(line.startsWith("0x")) line = line.substring(2);
                 BigInteger bigInteger = new BigInteger(line, 16);
                 String binString = bigInteger.toString(2);
+                binString = "00000000000000000000000000000000000000000000000000000000000"
+                        + "00000000000000000000000000000000000000000000000000000000000"
+                        + "00000000000000000000000000000000000000000000000000000000000"
+                        + "00000000000000000000000000000000000000000000000000000000000"
+                        + "0000000000" + binString;
+
+                binString = binString.substring(binString.length()-256);
 
                 for(int i=0; i< binString.length();i++) {
                     if(binString.substring(i,i+1).matches("1")) sum = sum+1;
                     else sum = sum -1;
                     count = count+1;
                 }
+
             }
-            Double obs = abs(sum)/sqrt(count);
-            Double erfc = Erf.erfc(obs/sqrt(2));
+
+            Double obs = abs(sum) / sqrt(count);
+            Double erfc = Erf.erfc(obs / sqrt(2));
             System.out.println("sum = " + sum.toString() + " count= " + count.toString()
                     + " obs = " + obs.toString() + " erf=" + erfc.toString());
         }
